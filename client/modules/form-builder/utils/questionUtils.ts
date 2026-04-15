83920f7cdc7ada24b8bf46090ca4d62cc2b9e5a4
Will-Go <wilsongongwu1@gmail.com>
2026-04-14 23:30:40 -0600

    feat: add the "dropdown" type to has options

--- client/modules/form-builder/utils/questionUtils.ts
+++ client/modules/form-builder/utils/questionUtils.ts
@@ -1,7 +1,7 @@
 import { Question } from "@/shared/types/forms";
 
 export function isOptionsQuestion(question: Question) {
-  return ["multiple_choice", "checkbox"].includes(question.type);
+  return ["multiple_choice", "checkbox", "dropdown"].includes(question.type);
 }
 
 export function hasRequiredQuestion(question: Question) {
